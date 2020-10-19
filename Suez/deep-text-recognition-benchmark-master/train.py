2b7c0d4adbe96a6638c71f551c505b318f5d2e48
root <scpkobayashi@gmail.com>
2020-10-19 14:25:00 +0000

    corrected inverted image files, added left zero digits in the labels.

--- Suez/deep-text-recognition-benchmark-master/train.py
+++ Suez/deep-text-recognition-benchmark-master/train.py
@@ -260,10 +260,10 @@ if __name__ == '__main__':
     parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
     parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
     parser.add_argument('--rgb', action='store_true', help='use rgb input')
-#     parser.add_argument('--character', type=str,
-#                         default='0123456789abcdefghijklmnopqrstuvwxyz', help='character label')
     parser.add_argument('--character', type=str,
-                        default='0123456789', help='character label')
+                        default='0123456789abcdefghijklmnopqrstuvwxyz', help='character label')
+#     parser.add_argument('--character', type=str,
+#                         default='0123456789', help='character label')
     parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode')
     parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
     parser.add_argument('--data_filtering_off', action='store_true', help='for data_filtering_off mode')
